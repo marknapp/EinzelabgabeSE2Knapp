78f2e0a8821320429059ee3dde4c4ab4f1b0f375
Markus Knapp <markus_knapp@Markuss-MacBook-Pro.local>
2021-03-13 15:31:44 +0100

    Button-Objekt etc. mit UI verknüpft und ClickListener für Button erstellt

--- app/src/main/java/com/example/einzelabgabese2knapp/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabese2knapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.einzelabgabese2knapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button b;
+    TextView serverAnswer;
+    EditText inputNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button);
+        serverAnswer = findViewById(R.id.textView);
+        inputNumber = findViewById(R.id.editTextNumber);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
